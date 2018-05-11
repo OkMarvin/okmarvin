b2531a75f5e0ee82f6a2d561da59c64b567ff876
chenxsan <chenxsan@gmail.com>
2018-05-11 15:47:35 +0800

    Add loadSiteConfig

---
+++ packages/okmarvin/lib/readData/defaultSiteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'OkMarvin',
+  description: 'OkMarvin is a Static Site Generator with better theme support',
+  author: 'Sam Chen',
+  lang: 'en',
+  theme: '@okmarvin/january',
+  toc: true,
+  permalink: '/:title'
+}
