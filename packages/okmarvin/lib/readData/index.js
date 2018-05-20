be52adf0ed955ba92d561590d40a28105f35f895
chenxsan <chenxsan@gmail.com>
2018-05-20 18:14:30 +0800

    Clean code

--- packages/okmarvin/lib/readData/index.js
+++ packages/okmarvin/lib/readData/index.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const glob = require('glob')
-const logger = require('@okmarvin/logger')
 const assert = require('assert')
 const fs = require('fs-extra')
 const async = require('neo-async')
@@ -13,7 +12,6 @@ const readMarkdown = require('./readMarkdown')
  * @param {Function} callback
  */
 module.exports = function (cwd, callback) {
-  logger.profile('Collect markdown files')
   const content = path.join(cwd, 'content')
   assert.ok(fs.pathExistsSync(content), `${content} folder doesn't exist`)
   const searchPattern = '{post,page}/**/*.md'
