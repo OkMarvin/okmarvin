7a8dbf4bd3ebc824fa6c778c3671ec2a6c04f9fd
chenxsan <chenxsan@gmail.com>
2018-05-20 18:04:33 +0800

    Refactor data

--- packages/okmarvin/lib/readData/readMarkdown.js
+++ packages/okmarvin/lib/readData/readMarkdown.js
@@ -3,8 +3,6 @@ const parseMarkdown = require('./parseMarkdown')
 module.exports = function (file, callback) {
   fs.readFile(file, 'utf8', (err, fileContent) => {
     if (err) return callback(err)
-    callback(null, {
-      [file]: parseMarkdown(fileContent)
-    })
+    callback(null, [file, parseMarkdown(fileContent)])
   })
 }
