473c57a0afaf6f3bf5f69072d84593ccf8a71856
chenxsan <chenxsan@gmail.com>
2018-06-06 16:16:53 +0800

    Update post template

--- packages/okmarvin-january/src/templates/post.js
+++ packages/okmarvin-january/src/templates/post.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import Helmet from 'react-helmet-async'
+import PropTypes from 'prop-types'
 export default class Post extends React.Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    siteConfig: PropTypes.shape({
+      lang: PropTypes.string.isRequired
+    })
+  }
   render () {
     return (
       <React.StrictMode>
-        hello post
+        <Helmet htmlAttributes={{
+          lang: this.props.siteConfig.lang
+        }}>
+          <meta charSet='utf-8' />
+          <title>{this.props.title}</title>
+        </Helmet>
+        <section
+          dangerouslySetInnerHTML={{
+            __html: this.props.content
+          }}
+          className='markdown'
+        />
       </React.StrictMode>
     )
   }
