859a7830b81e208d68e37c2bf2af3bc65cb496c2
chenxsan <chenxsan@gmail.com>
2018-06-06 16:19:17 +0800

    Add more meta tags

--- packages/okmarvin-january/src/templates/post.js
+++ packages/okmarvin-january/src/templates/post.js
@@ -16,6 +16,11 @@ export default class Post extends React.Component {
           lang: this.props.siteConfig.lang
         }}>
           <meta charSet='utf-8' />
+          <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1'
+          />
           <title>{this.props.title}</title>
         </Helmet>
         <section
