5fe6ca0976a97c2925be6a9d9cc1adb60b03fe6f
chenxsan <chenxsan@gmail.com>
2018-06-06 16:17:26 +0800

    Render data with template

--- packages/okmarvin/lib/render/index.js
+++ packages/okmarvin/lib/render/index.js
@@ -2,6 +2,7 @@ const ReactDOMServer = require('react-dom/server')
 const React = require('react')
 const async = require('neo-async')
 const path = require('path')
+const { HelmetProvider } = require('react-helmet-async')
 module.exports = function (data, callback) {
   const { files, siteConfig } = data
   const { theme } = siteConfig
@@ -13,9 +14,26 @@ module.exports = function (data, callback) {
         '..',
         file.template
       )).default
-      const html = ReactDOMServer.renderToStaticMarkup(
-        React.createElement(Component, {})
+      const helmetContext = {}
+      const rendered = ReactDOMServer.renderToStaticMarkup(
+        React.createElement(
+          HelmetProvider,
+          { context: helmetContext },
+          React.createElement(Component, { ...file, siteConfig })
+        )
       )
+      const { helmet } = helmetContext
+      const html = `<!doctype html>
+<html ${helmet.htmlAttributes.toString()}>
+  <head>
+    ${helmet.title.toString()}
+    ${helmet.meta.toString()}
+    ${helmet.link.toString()}
+  </head>
+  <body ${helmet.bodyAttributes.toString()}>
+    <div id="___OkMarvin___">${rendered}</div>
+  </body>
+</html>`
       callback(null, { ...file, html })
     },
     function (err, files) {
