a28e0dd72ab8b138220dfa3aa4073e4296404e72
chenxsan <chenxsan@gmail.com>
2018-06-07 23:01:54 +0800

    Remove Singular

--- packages/okmarvin-january/src/templates/page.js
+++ packages/okmarvin-january/src/templates/page.js
@@ -1,6 +1,29 @@
 import React from 'react'
-import Singular from '../components/Singular'
+import { Block } from 'jsxstyle'
 import PageContentHeader from '../components/PageContentHeader'
+import SingularHelmet from '../components/SingularHelmet'
+import Main from '../styled/Main'
+class Page extends React.Component {
+  render () {
+    return (
+      <React.StrictMode>
+        <Main>
+          <Block component='article'>
+            <PageContentHeader {...this.props} />
+            <section
+              dangerouslySetInnerHTML={{
+                __html: this.props.content
+              }}
+              className='okmarvin-content'
+            />
+          </Block>
+        </Main>
+      </React.StrictMode>
+    )
+  }
+}
 export default props => (
-  <Singular {...props} header={PageContentHeader} />
+  <SingularHelmet {...props}>
+    <Page {...props} />
+  </SingularHelmet>
 )
--- packages/okmarvin-january/src/templates/post.js
+++ packages/okmarvin-january/src/templates/post.js
@@ -1,6 +1,29 @@
 import React from 'react'
-import Singular from '../components/Singular'
+import { Block } from 'jsxstyle'
 import PostContentHeader from '../components/PostContentHeader'
+import SingularHelmet from '../components/SingularHelmet'
+import Main from '../styled/Main'
+class Post extends React.Component {
+  render () {
+    return (
+      <React.StrictMode>
+        <Main>
+          <Block component='article'>
+            <PostContentHeader {...this.props} />
+            <section
+              dangerouslySetInnerHTML={{
+                __html: this.props.content
+              }}
+              className='okmarvin-content'
+            />
+          </Block>
+        </Main>
+      </React.StrictMode>
+    )
+  }
+}
 export default props => (
-  <Singular {...props} header={PostContentHeader} />
+  <SingularHelmet {...props}>
+    <Post {...props} />
+  </SingularHelmet>
 )
