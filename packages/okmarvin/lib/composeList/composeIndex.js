d29c88fa854f843c0167c647b9d5908f7a639571
chenxsan <chenxsan@gmail.com>
2018-06-08 09:25:33 +0800

    Add paginate support

--- packages/okmarvin/lib/composeList/composeIndex.js
+++ packages/okmarvin/lib/composeList/composeIndex.js
@@ -2,16 +2,54 @@ const computeTemplate = require('../parseData/computeTemplate')
 const computeCss = require('../parseData/computeCss')
 module.exports = function (data, callback) {
   const { siteConfig, files } = data
-  // which data an index page would need?
-  callback(null, {
+  const { paginate } = siteConfig
+  const list = files
+    .filter(file => file.template === 'post.js')
+    .sort((a, b) => b.datePublished - a.datePublished)
+  const base = {
     title: siteConfig.title,
     description: siteConfig.description,
     author: siteConfig.author,
-    permalink: '',
     template: computeTemplate(siteConfig.themeManifest, 'index'),
     css: computeCss(siteConfig.themeManifest, 'index'),
-    list: files
-      .filter(file => file.template === 'post.js')
-      .sort((a, b) => b.datePublished - a.datePublished)
-  })
+    permalink: ''
+  }
+  // which data an index page would need?
+  if (!paginate) {
+    callback(null, [{
+      ...base,
+      list
+    }])
+  } else {
+    let num = Math.ceil(list.length / paginate)
+    if (num === 1) {
+      return callback(null, [{...base, list}])
+    }
+    let results = []
+    for (let i = 0; i < num; i++) {
+      if (i === 0) {
+        results = results.concat({
+          ...base,
+          list: list.slice(i * paginate, i * paginate + paginate),
+          paginator: {
+            current: i + 1,
+            total: num,
+            urlFormat: '/page:num'
+          }
+        })
+      } else {
+        results = results.concat({
+          ...base,
+          list: list.slice(i * paginate, i * paginate + paginate),
+          paginator: {
+            current: i + 1,
+            total: num,
+            urlFormat: '/page:num'
+          },
+          permalink: `/page${i + 1}`
+        })
+      }
+    }
+    callback(null, results)
+  }
 }
--- packages/okmarvin/lib/composeList/index.js
+++ packages/okmarvin/lib/composeList/index.js
@@ -4,17 +4,17 @@ module.exports = function (data, callback) {
   const { siteConfig, files } = data
   // const lists = ['index', 'topic', 'author', 'date']
   // figure out which list pages do we want to render
-  async.parallel(
-    [
-      callback => composeIndex(data, callback)
-    ],
-    function (err, results) {
-      if (err) return callback(err)
-      callback(null, {
-        ...data,
-        siteConfig,
-        files: files.concat(results)
-      })
-    }
-  )
+  async.parallel([callback => composeIndex(data, callback)], function (
+    err,
+    results
+  ) {
+    if (err) return callback(err)
+    callback(null, {
+      ...data,
+      siteConfig,
+      files: files.concat(
+        results.reduce((acc, result) => acc.concat(result), [])
+      )
+    })
+  })
 }
