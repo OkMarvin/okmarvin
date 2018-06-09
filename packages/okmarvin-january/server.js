e88e4c4e7a6ef15c5756e337ca583e028d88fc7b
chenxsan <chenxsan@gmail.com>
2018-06-09 16:31:39 +0800

    Add server.js

---
+++ packages/okmarvin-january/server.js
@@ -0,0 +1,55 @@
+const chokidar = require('chokidar')
+const serve = require('webpack-serve')
+const config = require('./webpack.config.js')
+const history = require('connect-history-api-fallback')
+const convert = require('koa-connect')
+const async = require('neo-async')
+const fs = require('fs-extra')
+const readData = require('@okmarvin/okmarvin/lib/readData')
+const parseData = require('@okmarvin/okmarvin/lib/parseData')
+const composeList = require('@okmarvin/okmarvin/lib/composeList')
+const guard = require('@okmarvin/okmarvin/lib/guard')
+const path = require('path')
+/**
+ *  Generate _data.json
+ * @param {function|null} callback
+ */
+function loadData (callback) {
+  async.waterfall(
+    [
+      callback => callback(null, __dirname),
+      readData,
+      parseData,
+      composeList,
+      guard
+    ],
+    (err, results) => {
+      if (err) return console.error(err)
+      fs.outputJSON(path.join(__dirname, '_data.json'), results, err => {
+        if (err) return console.error(err)
+        callback && callback(null)
+      })
+    }
+  )
+}
+loadData(() => {
+  serve({
+    config,
+    hot: {
+      host: 'localhost',
+      port: 8090
+    },
+    add: (app, middleware, options) => {
+      const historyOptions = {}
+      app.use(convert(history(historyOptions)))
+    }
+  }).then(server => {
+    server.on('listening', ({ server, options }) => {
+      console.log('i am running')
+    })
+    const watcher = chokidar.watch('./content')
+    watcher.on('all', () => {
+      loadData()
+    })
+  })
+})
--- packages/okmarvin-january/webpack.config.js
+++ packages/okmarvin-january/webpack.config.js
@@ -1,67 +1,44 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const fs = require('fs-extra')
-const readData = require('@okmarvin/okmarvin/lib/readData')
-const parseData = require('@okmarvin/okmarvin/lib/parseData')
-const composeList = require('@okmarvin/okmarvin/lib/composeList')
-const guard = require('@okmarvin/okmarvin/lib/guard')
-const async = require('neo-async')
-const webpackConfigPromise = new Promise((resolve, reject) => {
-  async.waterfall(
-    [
-      callback => callback(null, __dirname),
-      readData,
-      parseData,
-      composeList,
-      guard
-    ],
-    (err, results) => {
-      if (err) return reject(err)
-      fs.outputJSON(path.join(__dirname, '_data.json'), results, err => {
-        if (err) return reject(err)
-        const entry = {
-          main: './src/index'
-        }
-        if (fs.pathExistsSync(path.join('./src/client/index.js'))) {
-          entry['client'] = './src/client'
-        }
-        resolve({
-          entry,
-          mode: 'development',
-          output: {
-            publicPath: '/'
-          },
-          resolve: {
-            symlinks: true
-          },
-          module: {
-            rules: [
-              {
-                test: /\.js$/,
-                include: path.resolve(__dirname, 'src'),
-                use: [
-                  {
-                    loader: 'babel-loader'
-                  }
-                ]
-              },
-              {
-                test: /\.css$/,
-                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-              }
-            ]
-          },
-          plugins: [
-            new HtmlWebpackPlugin({
-              inject: false,
-              template: require('html-webpack-template'),
-              appMountId: '___OkMarvin___',
-              title: 'January for OkMarvin'
-            })
-          ]
-        })
-      })
-    }
-  )
-})
-module.exports = webpackConfigPromise
+const entry = {
+  main: './src/index'
+}
+if (fs.pathExistsSync(path.join('./src/client/index.js'))) {
+  entry['client'] = './src/client'
+}
+module.exports = {
+  entry,
+  mode: 'development',
+  output: {
+    publicPath: '/'
+  },
+  resolve: {
+    symlinks: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: require('html-webpack-template'),
+      appMountId: '___OkMarvin___',
+      title: 'January for OkMarvin'
+    })
+  ]
+}
