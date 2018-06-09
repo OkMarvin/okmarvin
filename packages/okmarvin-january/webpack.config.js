15c810b446ed68807f2f9817b836abf22c67caad
chenxsan <chenxsan@gmail.com>
2018-06-09 09:54:24 +0800

    Update webpack.config.js

--- packages/okmarvin-january/webpack.config.js
+++ packages/okmarvin-january/webpack.config.js
@@ -1,52 +1,78 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const fs = require('fs-extra')
-const entry = {
-  main: './src/index'
-}
-if (fs.pathExistsSync(path.join('./src/client/index.js'))) {
-  entry['client'] = './src/client'
-}
-module.exports = {
-  entry,
-  mode: 'development',
-  output: {
-    publicPath: '/'
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  resolve: {
-    symlinks: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, 'src'),
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              cacheDirectory: true,
-              plugins: ['react-hot-loader/babel', '@babel/plugin-proposal-object-rest-spread']
-            }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: require('html-webpack-template'),
-      appMountId: '___OkMarvin___',
-      title: 'January for OkMarvin'
-    })
-  ]
-}
+const readData = require('@okmarvin/okmarvin/lib/readData')
+const parseData = require('@okmarvin/okmarvin/lib/parseData')
+const composeList = require('@okmarvin/okmarvin/lib/composeList')
+const guard = require('@okmarvin/okmarvin/lib/guard')
+const async = require('neo-async')
+const webpackConfigPromise = new Promise((resolve, reject) => {
+  async.waterfall(
+    [
+      callback => callback(null, __dirname),
+      readData,
+      parseData,
+      composeList,
+      guard
+    ],
+    (err, results) => {
+      if (err) return reject(err)
+      fs.outputJSON(path.join(__dirname, '_data.json'), results, err => {
+        if (err) return reject(err)
+        const entry = {
+          main: './src/index'
+        }
+        if (fs.pathExistsSync(path.join('./src/client/index.js'))) {
+          entry['client'] = './src/client'
+        }
+        resolve({
+          entry,
+          mode: 'development',
+          output: {
+            publicPath: '/'
+          },
+          devServer: {
+            historyApiFallback: true
+          },
+          resolve: {
+            symlinks: false
+          },
+          module: {
+            rules: [
+              {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                  {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react'],
+                      cacheDirectory: true,
+                      plugins: [
+                        'react-hot-loader/babel',
+                        '@babel/plugin-proposal-object-rest-spread'
+                      ]
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+              }
+            ]
+          },
+          plugins: [
+            new HtmlWebpackPlugin({
+              inject: false,
+              template: require('html-webpack-template'),
+              appMountId: '___OkMarvin___',
+              title: 'January for OkMarvin'
+            })
+          ]
+        })
+      })
+    }
+  )
+})
+module.exports = webpackConfigPromise
