ab6a0f22285e73b551fa608629c79d071b9673d9
chenxsan <chenxsan@gmail.com>
2018-06-10 22:40:23 +0800

    Add template tag

---
+++ packages/okmarvin-january/src/templates/tag.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Html from '../components/Html'
+import Helmet from 'react-helmet-async'
+import PropTypes from 'prop-types'
+import Main from '../styled/Main'
+import ArchiveItem from '../components/ArchiveItem'
+import Pagination from '../components/Pagination'
+import PageContentHeader from '../components/PageContentHeader'
+class Tag extends React.Component {
+  render () {
+    const { title, list, siteConfig } = this.props
+    const { paginate } = siteConfig
+    return (
+      <React.Fragment>
+        <Helmet>
+          <meta property='og:type' content='website' />
+        </Helmet>
+        <Main>
+          {
+            <React.Fragment>
+              <PageContentHeader title={`Topic: ${title}`} />
+              {list.map(d => <ArchiveItem key={d.permalink} {...d} />)}
+            </React.Fragment>
+          }
+          {paginate &&
+            this.props.paginator && <Pagination {...this.props.paginator} />}
+        </Main>
+      </React.Fragment>
+    )
+  }
+}
+Tag.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  siteConfig: PropTypes.shape({
+    paginate: PropTypes.bool
+  }),
+  list: PropTypes.array,
+  paginator: PropTypes.object
+}
+export default props => (
+  <Html {...props}>
+    <Tag {...props} />
+  </Html>
+)
