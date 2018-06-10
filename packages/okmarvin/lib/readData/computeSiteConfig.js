6abf9456698922a8e79247564920bb38a358b558
chenxsan <chenxsan@gmail.com>
2018-06-10 19:45:03 +0800

    Normalize menu permalink

--- packages/okmarvin/lib/readData/computeSiteConfig.js
+++ packages/okmarvin/lib/readData/computeSiteConfig.js
@@ -1,19 +1,35 @@
 const yaml = require('js-yaml')
+const normalizePermalink = require('../parseData/computePermalink/normalizePermalink')
 module.exports = (yml, callback) => {
   const defaultSiteConfig = require('./defaultSiteConfig')
-  let siteConfig = {}
   try {
-    siteConfig = yaml.safeLoad(
-      yml
-    )
+    const siteConfig = yaml.safeLoad(yml)
+    if (!siteConfig) return callback(null, defaultSiteConfig)
     callback(
       null,
-      Object.assign({}, defaultSiteConfig, siteConfig)
+      Object.assign(
+        {},
+        defaultSiteConfig,
+        Object.assign(
+          {},
+          siteConfig,
+          siteConfig.menu
+            ? {
+              menu: siteConfig.menu.map(m => ({
+                ...m,
+                permalink: normalizePermalink(m.permalink)
+              }))
+            }
+            : {}
+        )
+      )
     )
   } catch (e) {
     if (e instanceof yaml.YAMLException) {
       // yaml syntax problem, should be fixed
       throw e
+    } else {
+      callback(e)
     }
   }
 }
