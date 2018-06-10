8ed4a6da8b17a234fe0e6d8325561eb4af332db2
chenxsan <chenxsan@gmail.com>
2018-06-10 14:47:21 +0800

    Encode title for chinese

--- packages/okmarvin/lib/parseData/computePermalink/replaceTitle.js
+++ packages/okmarvin/lib/parseData/computePermalink/replaceTitle.js
@@ -3,6 +3,6 @@ module.exports = function (permalink, data) {
   slugger.reset()
   return permalink.replace(
     /:title/g,
-    slugger.slug(data.title)
+    encodeURIComponent(slugger.slug(data.title))
   )
 }
