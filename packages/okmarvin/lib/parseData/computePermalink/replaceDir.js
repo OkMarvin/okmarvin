5fc4867d615370a39fc977a606b1a4881bc65af0
chenxsan <chenxsan@gmail.com>
2018-06-11 14:41:15 +0800

    Encode chinese

--- packages/okmarvin/lib/parseData/computePermalink/replaceDir.js
+++ packages/okmarvin/lib/parseData/computePermalink/replaceDir.js
@@ -1,10 +1,12 @@
 const path = require('path')
+const slug = require('@okmarvin/slug')
 module.exports = function (permalink, dir = '') {
   return permalink.replace(
     /:dir/g,
     dir
       .split(path.sep)
       .slice(1)
+      .map(d => encodeURIComponent(slug(d)))
       .join(path.sep)
   )
 }
--- packages/okmarvin/lib/parseData/computePermalink/replaceDir.test.js
+++ packages/okmarvin/lib/parseData/computePermalink/replaceDir.test.js
@@ -2,3 +2,13 @@ const fn = require('./replaceDir')
 test(':dir replaced', () => {
   expect(fn('/:dir', 'page/blog/hello')).toBe('/blog/hello')
 })
+test(':dir with chinese', () => {
+  expect(
+    fn('/:dir', 'page/博客/测试')
+  ).toBe(`/${encodeURIComponent('博客')}/${encodeURIComponent('测试')}`)
+})
+test(':dir with chinese and whitespace', () => {
+  expect(
+    fn('/:dir', 'page/博 客/测试')
+  ).toBe(`/${encodeURIComponent('博-客')}/${encodeURIComponent('测试')}`)
+})
