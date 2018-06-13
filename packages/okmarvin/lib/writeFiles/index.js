3942a559d8e46a010579d1a4ab074f956116faf9
chenxsan <chenxsan@gmail.com>
2018-06-13 16:10:15 +0800

    Clean code

--- packages/okmarvin/lib/writeFiles/index.js
+++ packages/okmarvin/lib/writeFiles/index.js
@@ -1,8 +1,6 @@
 const async = require('neo-async')
 const fs = require('fs-extra')
 const path = require('path')
-const writeFeed = require('./writeFeed')
-const writeSitemap = require('./writeSitemap')
 const logger = require('@okmarvin/logger')
 module.exports = function (data, callback) {
   const { files, cwd, destination } = data
@@ -23,9 +21,7 @@ module.exports = function (data, callback) {
             )
           },
           callback
-        ),
-      callback => writeFeed(data, callback),
-      callback => writeSitemap(data, callback)
+        )
     ],
     err => {
       if (err) return callback(err)
