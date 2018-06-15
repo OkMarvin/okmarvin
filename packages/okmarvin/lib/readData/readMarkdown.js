292f9f8c1d267cccd7450f9b8611eb3b1763f250
chenxsan <chenxsan@gmail.com>
2018-06-15 12:28:23 +0800

    Update variable name

--- packages/okmarvin/lib/readData/readMarkdown.js
+++ packages/okmarvin/lib/readData/readMarkdown.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const parseMarkdown = require('./parseMarkdown')
-module.exports = function (file, callback) {
-  fs.readFile(file, 'utf8', (err, fileContent) => {
+module.exports = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, fileContent) => {
     if (err) return callback(err)
-    callback(null, [file, parseMarkdown(fileContent)])
+    callback(null, [filePath, parseMarkdown(fileContent)])
   })
 }
