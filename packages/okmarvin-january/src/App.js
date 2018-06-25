278742fe2de5abb4c7e6410e3b5520dc862e5491
chenxsan <chenxsan@gmail.com>
2018-06-25 10:12:42 +0800

    Optimize App

--- packages/okmarvin-january/src/App.js
+++ packages/okmarvin-january/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { Router } from '@reach/router'
 import REACT_DATA from '../_data.json'
+const Components = {}
+function importAll (r) {
+  r.keys().forEach(key => (Components[key] = r(key)))
+}
+importAll(require.context('./templates', true, /\.js$/))
 class App extends React.Component {
   render () {
     const { files, siteConfig } = REACT_DATA
     return (
       <Router id='___OkMarvin___'>
         {files.map(file => {
-          const Component = require(`./templates/${file.template}`).default
+          const Component = Components['./' + file.template].default
           return (
             <Component
               key={file.permalink}
