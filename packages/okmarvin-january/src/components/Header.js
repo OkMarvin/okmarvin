9b5af0e901df846d7f3b599e03578e010df6fd9c
chenxsan <chenxsan@gmail.com>
2018-06-28 23:34:56 +0800

    Fix logo

--- packages/okmarvin-january/src/components/Header.js
+++ packages/okmarvin-january/src/components/Header.js
@@ -33,7 +33,7 @@ const Header = props => (
         <React.Fragment>
           <Inline
             component='img'
-            width={42}
+            maxHeight={42}
             fontSize={14}
             border='none'
             marginRight={10}
