a5877b8f4d0fe02a29feed008f8c2fcb0f184485
chenxsan <chenxsan@gmail.com>
2018-12-21 21:58:09 +0800

    Remove unused module

--- packages/okmarvin/lib/guard/ensureMiniTemplates.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (manifest) {
-  // theme should at least has three templates
-  ['index.js', 'post.js', 'page.js'].forEach(t => {
-    if (!manifest[t]) throw new Error(`template ${t} is missing`)
-  })
-}
