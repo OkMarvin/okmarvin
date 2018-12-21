7d0fe1cd8c1e43f3e3ed190d533aab26f532bb0c
chenxsan <chenxsan@gmail.com>
2018-12-21 16:47:53 +0800

    Extract schema

--- packages/okmarvin/lib/read/readMarkdown.js
+++ packages/okmarvin/lib/read/readMarkdown.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const matter = require('gray-matter')
 const ajv = require('../helpers/ajv')
 const TOML = require('@iarna/toml')
+const fileSchema = require('../schemas/file')
 module.exports = function (filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, fileContent) => {
@@ -15,48 +16,7 @@ module.exports = function (filePath) {
       })
       if (
         !ajv.validate(
-          {
-            required: ['content', 'data', 'isEmpty'],
-            properties: {
-              content: {
-                type: 'string'
-              },
-              isEmpty: {
-                const: false // front matter can't be empty
-              },
-              data: {
-                required: ['title', 'date'],
-                properties: {
-                  title: {
-                    type: 'string'
-                  },
-                  author: {
-                    type: 'string'
-                  },
-                  excerpt: {
-                    type: 'string'
-                  },
-                  date: {
-                    type: ['string', 'object']
-                  },
-                  dateModified: {
-                    type: ['string', 'object']
-                  },
-                  permalink: {
-                    type: 'string'
-                  },
-                  toc: {
-                    type: 'boolean'
-                  },
-                  tags: {
-                    items: {
-                      type: 'string'
-                    }
-                  }
-                }
-              }
-            }
-          },
+          fileSchema,
           file
         )
       ) {
