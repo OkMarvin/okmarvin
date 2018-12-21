a22c37c0db3c081c68d169c5a866df03a0b578f7
chenxsan <chenxsan@gmail.com>
2018-12-21 20:18:16 +0800

    Remove tests

--- packages/okmarvin/lib/read/__mocks__/readThemeManifest.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (siteConfig, callback) {
-  callback(null, { ...siteConfig, themeManifest: {} })
-}
--- packages/okmarvin/lib/read/__mocks__/readUserSiteConfig.js
+++
@@ -1,3 +0,0 @@
-module.exports = (filePath, callback) => {
-  callback(null, '')
-}
