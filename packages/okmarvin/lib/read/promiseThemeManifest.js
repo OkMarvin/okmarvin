4306e3a649e1ef374b5eb1c05186f3b0e1490af8
chenxsan <chenxsan@gmail.com>
2018-12-22 12:37:59 +0800

    Just use fs

--- packages/okmarvin/lib/read/promiseThemeManifest.js
+++ packages/okmarvin/lib/read/promiseThemeManifest.js
@@ -1,13 +1,13 @@
 // read theme manifest json file
 // then merge with siteConfig object
-const fse = require('fs-extra')
+const fs = require('fs')
 const requireResolve = require('../helpers/requireResolve')
 const ajv = require('../helpers/ajv')
 const themeSchema = require('../schemas/theme')
 module.exports = function (root, theme) {
   return new Promise((resolve, reject) => {
     const themeRoot = requireResolve(theme, { paths: [root] })
-    fse.readFile(themeRoot, 'utf8', (err, manifestStr) => {
+    fs.readFile(themeRoot, 'utf8', (err, manifestStr) => {
       if (err) return reject(err)
       const theme = JSON.parse(manifestStr)
       if (!ajv.validate(themeSchema, theme)) {
