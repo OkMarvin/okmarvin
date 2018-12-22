e1a5dc991310175fa389c780daaa8c38d328899c
chenxsan <chenxsan@gmail.com>
2018-12-22 22:28:54 +0800

    Add description

--- packages/okmarvin/lib/compose/compose404.js
+++ packages/okmarvin/lib/compose/compose404.js
@@ -1,11 +1,15 @@
 module.exports = function (conn, callback) {
   const { siteConfig } = conn
   const { themeManifest } = siteConfig
+  // if there's no 404.js template, then don't render one
   if (!themeManifest['404.js']) return callback(null, [])
-  callback(null, [{
-    permalink: '/404.html',
-    template: '404.js',
-    css: '404.css',
-    title: 'Page Not Found'
-  }])
+  callback(null, [
+    {
+      permalink: '/404.html',
+      template: '404.js',
+      css: '404.css',
+      title: 'Page Not Found',
+      description: 'The page does not exist'
+    }
+  ])
 }
