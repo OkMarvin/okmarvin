9fc98dfc628e6d5967fff9e05133acc224aa3d4a
chenxsan <chenxsan@gmail.com>
2018-12-22 13:17:09 +0800

    Add test for promiseFileData

--- __mocks__/gray-matter.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-let result = null
-const matter = jest.fn(() => result)
-matter.__setResult = (v) => {
-  result = v
-}
-module.exports = matter
---
+++ packages/okmarvin/lib/read/promiseFileData.test.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const promiseFileData = require('./promiseFileData')
+test('returns array', () => {
+  const filePath = path.join(
+    __dirname,
+    'fixtures',
+    'content/page/about-okmarvin.md'
+  )
+  return promiseFileData(filePath).then(data => {
+    expect(data).toEqual([
+      filePath,
+      {
+        content: 'this is content',
+        data: {
+          title: 'about okmarvin',
+          date: new Date('2019-10-10')
+        },
+        excerpt: '',
+        isEmpty: false
+      }
+    ])
+  })
+})
