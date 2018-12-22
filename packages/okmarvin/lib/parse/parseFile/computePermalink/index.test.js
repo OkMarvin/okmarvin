355309610a1fa3b057f5759a9e69c828651547bf
chenxsan <chenxsan@gmail.com>
2018-12-22 21:27:33 +0800

    Add snapshot

--- packages/okmarvin/lib/parse/parseFile/computePermalink/index.test.js
+++ packages/okmarvin/lib/parse/parseFile/computePermalink/index.test.js
@@ -1,11 +1,11 @@
 const fn = require('./index')
 test('returns permalink', () => {
-  expect(
-    fn(
-      '/:month/:title',
-      { categories: [], title: 'okmarvin' },
-      new Date('2010-10-10'),
-      'content/page/hello'
-    )
-  ).toBe('/10/okmarvin/')
+  const data = fn(
+    '/:month/:title',
+    { categories: [], title: 'okmarvin' },
+    new Date('2010-10-10'),
+    'content/page/hello'
+  )
+  expect(data).toBe('/10/okmarvin/')
+  expect(data).toMatchSnapshot()
 })
