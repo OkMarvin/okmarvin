4391f46871b945b0131583b5436c4e2e0988c754
chenxsan <chenxsan@gmail.com>
2018-12-24 12:54:53 +0800

    Refactor

--- packages/okmarvin/lib/index.js
+++ packages/okmarvin/lib/index.js
@@ -8,12 +8,13 @@ const render = require('./render')
 const write = require('./write')
 const copy = require('./copy')
 /**
- * Static site generator
- * @param {string} cwd Current working directory
- * @param {string} source Content source directory
- * @param {string} destination Build target
+ * Marvin, the static site generator
  */
-module.exports = function (source = 'content', destination = 'dist') {
+module.exports = function ({
+  source = 'content',
+  destination = 'dist',
+  devHook = false // we can hook into waterfall with devHook
+} = {}) {
   logger.profile('SSG')
   const conn = {
     root: process.cwd(),
@@ -21,16 +22,14 @@ module.exports = function (source = 'content', destination = 'dist') {
     to: destination,
     builtAt: Date.now()
   }
-  async.waterfall([
-    (callback) => callback(null, conn),
-    read,
-    parse,
-    compose,
-    guard,
-    render,
-    write,
-    copy
-  ], (err, results) => {
+  let tasks = [callback => callback(null, conn), read, parse, compose, guard]
+  if (devHook === false) {
+    tasks = tasks.concat([render, write, copy])
+  } else {
+    // const devHook = (conn, callback) => {}
+    tasks = tasks.concat([devHook])
+  }
+  async.waterfall(tasks, (err, results) => {
     logger.profile('SSG')
     if (err) throw err
   })
