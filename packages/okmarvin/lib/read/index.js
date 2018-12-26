6166fbbd37beec6bb2da03bea85b1c48df9d0361
chenxsan <chenxsan@gmail.com>
2018-12-26 19:30:59 +0800

    Fix bug

--- packages/okmarvin/lib/read/index.js
+++ packages/okmarvin/lib/read/index.js
@@ -29,77 +29,96 @@ module.exports = async function (conn, callback) {
       `Oops, nothing to do because "${from}" directory does not exist.`
     )
   }
-  async.parallel(
-    {
-      okmarvinConfig: async callback => {
-        const [err, okmarvinConfig] = await promiseCatcher(
-          promiseOkmarvinConfig(root)
-        )
-        if (!okmarvinConfig) {
-          return callback(err)
-        }
-        callback(null, okmarvinConfig)
-      },
-      siteConfig: async callback => {
-        const [err, userSiteConfig] = await promiseCatcher(
-          promiseUserSiteConfig(path.join(root, '_config.toml'))
-        )
-        if (!userSiteConfig) {
-          // oops something wrong with _config.toml
-          return callback(err)
-        }
-        // here we want to make sure _config.toml has correct data
-        if (!ajv.validate(siteConfigSchema, userSiteConfig)) {
-          return console.log(
-            'You have invalid configuration in _config.toml:\n',
-            ajv.errors
+  async.waterfall(
+    [
+      callback =>
+        async.parallel(
+          {
+            okmarvinConfig: async callback => {
+              const [err, okmarvinConfig] = await promiseCatcher(
+                promiseOkmarvinConfig(root)
+              )
+              if (!okmarvinConfig) {
+                return callback(err)
+              }
+              callback(null, okmarvinConfig)
+            },
+            siteConfig: async callback => {
+              const [err, userSiteConfig] = await promiseCatcher(
+                promiseUserSiteConfig(path.join(root, '_config.toml'))
+              )
+              if (!userSiteConfig) {
+                // oops something wrong with _config.toml
+                return callback(err)
+              }
+              // here we want to make sure _config.toml has correct data
+              if (!ajv.validate(siteConfigSchema, userSiteConfig)) {
+                return console.log(
+                  'You have invalid configuration in _config.toml:\n',
+                  ajv.errors
+                )
+              }
+              const siteConfig = { ...defaultSiteConfig, ...userSiteConfig }
+              const [
+                readThemeManifestErr,
+                themeManifest
+              ] = await promiseCatcher(
+                promiseThemeManifest(root, siteConfig.theme)
+              )
+              if (!themeManifest) {
+                return callback(readThemeManifestErr)
+              }
+              callback(null, { ...siteConfig, themeManifest: themeManifest })
+            },
+            files: async callback => {
+              // we might need pattern matching to catch error here
+              // https://github.com/tc39/proposal-pattern-matching
+              const [readFilesPathErr, filesPath] = await promiseCatcher(
+                promiseFilesPath(absoluteContentPath)
+              )
+              if (!filesPath) {
+                return callback(readFilesPathErr)
+              }
+              const [readMarkdownErr, files] = await promiseCatcher(
+                Promise.all(
+                  filesPath.map(filePath => promiseFileData(filePath))
+                )
+              )
+              if (!files) {
+                return callback(readMarkdownErr)
+              }
+              callback(null, files)
+            }
+          },
+          callback
+        ),
+      (results, callback) => {
+        const {
+          siteConfig: { layoutHierarchy }
+        } = results
+        const findMe = [
+          ...new Set(
+            Object.values(layoutHierarchy).reduce(function (flat, toFlatten) {
+              return flat.concat(toFlatten)
+            }, [])
           )
-        }
-        const siteConfig = { ...defaultSiteConfig, ...userSiteConfig }
-        const [readThemeManifestErr, themeManifest] = await promiseCatcher(
-          promiseThemeManifest(root, siteConfig.theme)
-        )
-        if (!themeManifest) {
-          return callback(readThemeManifestErr)
-        }
-        callback(null, { ...siteConfig, themeManifest: themeManifest })
-      },
-      files: async callback => {
-        // we might need pattern matching to catch error here
-        // https://github.com/tc39/proposal-pattern-matching
-        const [readFilesPathErr, filesPath] = await promiseCatcher(
-          promiseFilesPath(absoluteContentPath)
-        )
-        if (!filesPath) {
-          return callback(readFilesPathErr)
-        }
-        const [readMarkdownErr, files] = await promiseCatcher(
-          Promise.all(filesPath.map(filePath => promiseFileData(filePath)))
-        )
-        if (!files) {
-          return callback(readMarkdownErr)
-        }
-        callback(null, files)
-      },
-      layouts: callback => {
+        ]
         const layouts = {}
         const layoutPath = path.join(__dirname, '..', 'layout')
-        fs.readdir(layoutPath, (err, files) => {
-          if (err) return callback(err)
-          files
-            .filter(file => file.endsWith('.js'))
-            .forEach(file => {
-              // first resolve root/layout
-              // then resolve __dirname/layout
-              const layout = requireResolve(file, {
-                paths: [path.join(root, 'layout'), layoutPath]
-              })
-              layouts[file] = require(layout)
+        findMe.forEach(file => {
+          // first resolve root/layout
+          try {
+            const layout = requireResolve(file, {
+              paths: [path.join(root, 'layout'), layoutPath]
             })
-          callback(null, layouts)
+            layouts[file] = require(layout)
+          } catch (_err) {
+            // do nothing
+          }
         })
+        callback(null, { ...results, layouts })
       }
-    },
+    ],
     (err, results) => {
       if (err) return callback(err)
       logger.success(`Read in ${prettyTime(Date.now() - begin)}`)
