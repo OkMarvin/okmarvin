ec83d108421c49bb9f9cd5a6234772ec7f6ddd85
chenxsan <chenxsan@gmail.com>
2018-12-28 10:02:27 +0800

    Make it faster

--- packages/okmarvin/lib/copy/index.js
+++ packages/okmarvin/lib/copy/index.js
@@ -3,10 +3,10 @@ const copyThemeStatic = require('./copyThemeStatic')
 const copyPublic = require('./copyPublic')
 const async = require('neo-async')
 module.exports = function (conn, callback) {
-  async.waterfall([
+  async.parallel([
     callback => copyFileAssets(conn, callback),
-    copyThemeStatic,
-    copyPublic
+    callback => copyThemeStatic(conn, callback),
+    callback => copyPublic(conn, callback)
   ], (err, results) => {
     if (err) return callback(err)
     return callback(null, conn)
