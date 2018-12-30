6f596759c54ac10ce59abb74c3f63f49c31a2ba7
chenxsan <chenxsan@gmail.com>
2018-12-30 14:02:19 +0800

    Collect layout hash

--- packages/okmarvin/lib/read/readLayouts.js
+++ packages/okmarvin/lib/read/readLayouts.js
@@ -1,10 +1,14 @@
 const requireResolve = require('../helpers/requireResolve')
 const path = require('path')
+const fs = require('fs')
+const async = require('neo-async')
+const { getHashDigest } = require('loader-utils')
 module.exports = (conn, callback) => {
   const { root } = conn
   const {
     siteConfig: { layoutHierarchy }
   } = conn
+  // all available layouts
   const findMe = [
     ...new Set(
       Object.values(layoutHierarchy).reduce(function (flat, toFlatten) {
@@ -12,18 +16,60 @@ module.exports = (conn, callback) => {
       }, [])
     )
   ]
-  const layouts = {}
   const layoutPath = path.join(__dirname, '..', 'layout')
-  findMe.forEach(file => {
-    // first resolve root/layout
-    try {
-      const layout = requireResolve(file, {
-        paths: [path.join(root, 'layout'), layoutPath]
-      })
-      layouts[file] = require(layout)
-    } catch (_err) {
-      // do nothing
+  async.parallel(
+    {
+      layouts: callback => {
+        const layouts = {}
+        async.each(
+          findMe,
+          (file, callback) => {
+            // first resolve root/layout
+            try {
+              const layout = requireResolve(file, {
+                paths: [path.join(root, 'layout'), layoutPath] // user could customize their layout
+              })
+              layouts[file] = require(layout)
+              callback()
+            } catch (_err) {
+              // do nothing
+              callback()
+            }
+          },
+          err => {
+            if (err) return callback(err)
+            callback(null, layouts)
+          }
+        )
+      },
+      layoutHash: callback => {
+        let layoutHash = []
+        async.each(
+          findMe,
+          (file, callback) => {
+            try {
+              const layout = requireResolve(file, {
+                paths: [path.join(root, 'layout'), layoutPath]
+              })
+              fs.readFile(layout, (err, data) => {
+                if (err) return callback(err)
+                layoutHash = layoutHash.concat([getHashDigest(data)])
+                callback()
+              })
+            } catch (err) {
+              callback()
+            }
+          },
+          err => {
+            if (err) return callback(err)
+            callback(null, layoutHash.sort())
+          }
+        )
+      }
+    },
+    (err, results) => {
+      if (err) return callback(err)
+      callback(null, { ...conn, ...results })
     }
-  })
-  callback(null, { ...conn, layouts })
+  )
 }
