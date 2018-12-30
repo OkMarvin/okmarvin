af4547afd2df2b846d6e3aeea3e3d2019e436c44
chenxsan <chenxsan@gmail.com>
2018-12-30 20:02:55 +0800

    Support .markdown extension

--- packages/okmarvin/lib/read/promiseFilesPath.js
+++ packages/okmarvin/lib/read/promiseFilesPath.js
@@ -1,7 +1,7 @@
 const glob = require('glob')
 module.exports = function (content) {
   return new Promise((resolve, reject) => {
-    const searchPattern = '{post,page}/**/*.md'
+    const searchPattern = '{post,page}/**/*.{md,markdown}'
     const opts = { cwd: content, absolute: true }
     glob(searchPattern, opts, (err, files) => {
       if (err) return reject(err)
