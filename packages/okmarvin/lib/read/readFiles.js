eb7a79f3487754175f86c1d861567e857d159922
chenxsan <chenxsan@gmail.com>
2018-12-30 20:03:18 +0800

    Count files

--- packages/okmarvin/lib/read/readFiles.js
+++ packages/okmarvin/lib/read/readFiles.js
@@ -2,6 +2,7 @@ const path = require('path')
 const promiseCatcher = require('../helpers/promiseCatcher')
 const promiseFileData = require('./promiseFileData')
 const promiseFilesPath = require('./promiseFilesPath')
+const logger = require('@parcel/logger')
 
 module.exports = async ({ root, from }, callback) => {
   const result = await promiseCatcher(promiseFilesPath(path.join(root, from)))
@@ -14,5 +15,6 @@ module.exports = async ({ root, from }, callback) => {
   if (filesResult.length === 1) {
     return callback(filesResult[0])
   }
+  logger.verbose(`Collected ${filesResult[1].length} markdown files`)
   callback(null, filesResult[1])
 }
