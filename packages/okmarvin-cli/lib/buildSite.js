d66f2873f6dc7d9c7c51a277bd275459d08aff20
chenxsan <chenxsan@gmail.com>
2018-12-30 19:07:18 +0800

    Clean code

--- packages/okmarvin-cli/lib/buildSite.js
+++ packages/okmarvin-cli/lib/buildSite.js
@@ -1,23 +1,5 @@
-const path = require('path')
-const fs = require('fs-extra')
 const okmarvin = require('@okmarvin/okmarvin')
-const logger = require('@parcel/logger')
-module.exports = function (cli) {
+module.exports = function ({ flags }) {
   process.env.NODE_ENV = 'production'
-  const {
-    clean = true,
-    logLevel = 3,
-    source = 'content',
-    destination = 'dist'
-  } = cli.flags
-  const cwd = process.cwd()
-  if (clean === true) {
-    // clean destination first
-    fs.remove(path.join(cwd, destination), err => {
-      if (err) return logger.error(err)
-      okmarvin({ logLevel, source, destination, clean })
-    })
-  } else {
-    okmarvin({ logLevel, source, destination, clean })
-  }
+  okmarvin(flags)
 }
