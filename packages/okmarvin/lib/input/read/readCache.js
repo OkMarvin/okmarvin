6e4bb393bd8639d22533638bc856716fc9eb4abb
chenxsan <chenxsan@gmail.com>
2018-12-31 16:21:34 +0800

    Optimize code

--- packages/okmarvin/lib/input/read/readCache.js
+++ packages/okmarvin/lib/input/read/readCache.js
@@ -3,14 +3,10 @@ const path = require('path')
 module.exports = ({ root }, callback) => {
   // cache for better build performance when `clean` option set to false
   // will be removed if not work
-  fs.readJson(path.join(root, '_cache.json'), (err, data) => {
-    if (err) {
-      return callback(null, {
-        lastThemeManifest: {},
-        layoutHash: [],
-        files: []
-      })
-    } // return a default one
-    return callback(null, data)
-  })
+  try {
+    const data = fs.readJsonSync(path.join(root, '_cache.json'))
+    callback(null, data)
+  } catch (err) {
+    return callback(err)
+  }
 }
