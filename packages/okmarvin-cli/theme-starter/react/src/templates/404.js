1acb6a063b70f478e3717e27ae4fa2d10dfe8e23
chenxsan <chenxsan@gmail.com>
2019-01-04 20:29:25 +0800

    Optimize templates

--- packages/okmarvin-cli/theme-starter/react/src/templates/404.js
+++ packages/okmarvin-cli/theme-starter/react/src/templates/404.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../../node_modules/sanitize.css'
 import '../../node_modules/prismjs/themes/prism-okaidia.css'
-export default (props) => (
-  <div>This is 404 template, you can use data from `props` to render your page.</div>
-)
+export default (props) => {
+  return (
+    <div>This is 404 template, you can use data from `props` to render your page.</div>
+  )
+}
