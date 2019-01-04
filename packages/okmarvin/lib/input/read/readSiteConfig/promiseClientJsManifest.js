0a93e742da99260a10cf866b52ec16aa5f557bbd
chenxsan <chenxsan@gmail.com>
2019-01-04 21:04:57 +0800

    Catch error

--- packages/okmarvin/lib/input/read/readSiteConfig/promiseClientJsManifest.js
+++ packages/okmarvin/lib/input/read/readSiteConfig/promiseClientJsManifest.js
@@ -3,11 +3,15 @@ const fs = require('fs')
 const path = require('path')
 module.exports = function (root, theme) {
   return new Promise((resolve, reject) => {
-    const themeRoot = require.resolve(theme, { paths: [root] })
-    fs.readFile(path.join(themeRoot, '..', 'static/js/manifest.json'), 'utf8', (err, manifestStr) => {
-      if (err) return resolve({})
-      const manifest = JSON.parse(manifestStr)
-      resolve(manifest)
-    })
+    try {
+      const themeRoot = require.resolve(theme, { paths: [root] })
+      fs.readFile(path.join(themeRoot, '..', 'static/js/manifest.json'), 'utf8', (err, manifestStr) => {
+        if (err) return resolve({})
+        const manifest = JSON.parse(manifestStr)
+        resolve(manifest)
+      })
+    } catch (err) {
+      resolve({})
+    }
   })
 }
--- packages/okmarvin/lib/input/read/readSiteConfig/promiseThemeManifest.js
+++ packages/okmarvin/lib/input/read/readSiteConfig/promiseThemeManifest.js
@@ -5,14 +5,18 @@ const ajv = require('../../../helpers/ajv')
 const themeSchema = require('../../../schemas/theme')
 module.exports = function (root, theme) {
   return new Promise((resolve, reject) => {
-    const themeRoot = require.resolve(theme, { paths: [root] })
-    fs.readFile(themeRoot, 'utf8', (err, manifestStr) => {
-      if (err) return resolve({})
-      const theme = JSON.parse(manifestStr)
-      if (!ajv.validate(themeSchema, theme)) {
-        return reject(ajv.errors)
-      }
-      resolve(theme)
-    })
+    try {
+      const themeRoot = require.resolve(theme, { paths: [root] })
+      fs.readFile(themeRoot, 'utf8', (err, manifestStr) => {
+        if (err) return resolve({})
+        const theme = JSON.parse(manifestStr)
+        if (!ajv.validate(themeSchema, theme)) {
+          return reject(ajv.errors)
+        }
+        resolve(theme)
+      })
+    } catch (err) {
+      resolve({})
+    }
   })
 }
