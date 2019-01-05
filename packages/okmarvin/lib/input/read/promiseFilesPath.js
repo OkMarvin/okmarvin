d279b93fe2e8a78227a27092346def613688cbd3
chenxsan <chenxsan@gmail.com>
2019-01-05 16:11:45 +0800

    Add comment

--- packages/okmarvin/lib/input/read/promiseFilesPath.js
+++ packages/okmarvin/lib/input/read/promiseFilesPath.js
@@ -2,6 +2,7 @@ const glob = require('glob')
 module.exports = function (content) {
   return new Promise((resolve, reject) => {
     const searchPattern = '{post,page}/**/*.{md,markdown}'
+    // TODO we might add `cache` to save some fs calls
     const opts = { cwd: content, absolute: true, nodir: true, nosort: true }
     try {
       const files = glob.sync(searchPattern, opts)
