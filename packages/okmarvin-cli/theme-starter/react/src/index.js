afbd9e93a7a1f5742bf848540ca7fab0be573c09
chenxsan <chenxsan@gmail.com>
2019-01-05 22:02:15 +0800

    Update boilerplate

--- packages/okmarvin-cli/theme-starter/react/src/index.js
+++ packages/okmarvin-cli/theme-starter/react/src/index.js
@@ -1,32 +1,51 @@
-/**
- * You don't need to care this boilerplate
- * Just focus on `src/templates` directory and client js index `src/client` directory
- */
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { Router } from '@reach/router'
 import { files, siteConfig } from '../_data.json'
 import Components from './templates/*.js'
-ReactDOM.render(
-  <Router id='___OkMarvin___'>
-    {files
-      .filter(file => file.template) // exclude xml files
-      .map(file => {
-        try {
-          const Component = Components[file.template.replace('.js', '')].default
-          return (
-            <Component
-              key={file.permalink}
-              path={file.permalink}
-              {...file}
-              siteConfig={siteConfig}
-              default={file.template === '404.js'}
-            />
-          )
-        } catch (err) {
-          return undefined
-        }
-      })}
-  </Router>,
-  document.getElementById('app')
+const root = document.getElementById('app')
+
+class ErrorBoundary extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+  componentDidCatch (error, info) {
+    if (error) console.error(error)
+    this.setState({ hasError: true })
+  }
+
+  render () {
+    if (this.state.hasError) {
+      return <h1>Something went wrong. Please refresh the page.</h1>
+    }
+    return this.props.children
+  }
+}
+
+render(
+  <ErrorBoundary>
+    <Router id='___OkMarvin___'>
+      {files
+        .filter(file => file.template) // exclude xml files
+        .map(file => {
+          try {
+            const Component =
+              Components[file.template.replace('.js', '')].default
+            return (
+              <Component
+                key={file.permalink}
+                path={file.permalink}
+                {...file}
+                siteConfig={siteConfig}
+                default={file.template === '404.js'}
+              />
+            )
+          } catch (err) {
+            return undefined
+          }
+        })}
+    </Router>
+  </ErrorBoundary>,
+  root
 )
