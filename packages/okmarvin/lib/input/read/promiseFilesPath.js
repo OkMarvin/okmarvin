e7a7e3a0fc601868767fb3f3016f72e24e761dee
chenxsan <chenxsan@gmail.com>
2019-01-05 15:44:11 +0800

    Update glob option

--- packages/okmarvin/lib/input/read/promiseFilesPath.js
+++ packages/okmarvin/lib/input/read/promiseFilesPath.js
@@ -2,7 +2,7 @@ const glob = require('glob')
 module.exports = function (content) {
   return new Promise((resolve, reject) => {
     const searchPattern = '{post,page}/**/*.{md,markdown}'
-    const opts = { cwd: content, absolute: true }
+    const opts = { cwd: content, absolute: true, nodir: true, nosort: true }
     try {
       const files = glob.sync(searchPattern, opts)
       resolve(files)
