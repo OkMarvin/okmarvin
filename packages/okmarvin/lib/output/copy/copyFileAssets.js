8eae07dd1c70b2de8220c77b64aa54800156b5b4
chenxsan <chenxsan@gmail.com>
2019-01-10 15:00:45 +0800

    Optimize performance

--- packages/okmarvin/lib/output/copy/copyFileAssets.js
+++ packages/okmarvin/lib/output/copy/copyFileAssets.js
@@ -1,49 +1,63 @@
-const async = require('neo-async')
+const { performance } = require('perf_hooks')
 const path = require('path')
 const glob = require('glob')
 const fs = require('fs-extra')
+const logger = require('@parcel/logger')
+const { prettyTime } = require('@okmarvin/helpers')
+const async = require('neo-async')
+
 module.exports = function (conn, callback) {
-  const { root, dest, files } = conn
-  async.each(
-    files,
-    function (file, callback) {
-      // if file is right under post/page directory, assume no file assets
-      // needed to be copied, they should be put inside own directory
-      if (!file.filePath) {
-        // composed list page
-        // no need to copy
-        return callback(null)
-      }
-      if (
-        ['post', 'page'].includes(path.basename(path.join(file.filePath, '..')))
-      ) {
-        return callback(null)
-      } else {
-        glob(
-          path.join(file.filePath, '..', '/*'),
-          {
-            ignore: ['**/*.md']
-          },
-          (err, assets) => {
-            if (err) return callback(err)
-            if (!assets.length) return callback(null)
-            async.parallel(
-              assets.map(
-                asset =>
-                  function (callback) {
-                    const basename = path.basename(asset)
-                    const target = path.join(root, dest, file.permalink, basename)
-                    fs.copy(asset, target, callback)
-                  }
-              ),
-              callback
-            )
-          }
-        )
-      }
-    },
-    err => {
+  const begin = performance.now()
+  const { root, dest, files, source } = conn
+  // find all dirs where assets are allowed
+  const dirObj = {}
+  const dirs = files
+    .filter(file => file.filePath)
+    .map(file => [path.join(file.filePath, '..'), file.permalink])
+    .filter(([dir, _]) => {
+      if (dir === path.join(conn.root, source, 'post')) return false
+      if (dir === path.join(conn.root, source, 'page')) return false
+      return true
+    })
+    .map(([dir, permalink]) => {
+      dirObj[dir] = permalink
+      return dir
+    })
+  let assets = glob
+    .sync('{post,page}/**/*', {
+      cwd: path.join(root, source),
+      absolute: true,
+      nosort: true
+    })
+    .filter(asset => !asset.endsWith('.md'))
+    .filter(
+      asset => !['post', 'page'].includes(path.basename(path.join(asset, '..')))
+    )
+    .filter(asset => {
+      return dirs.find(dir => {
+        if (path.join(asset, '..') === dir) {
+          return true
+        }
+        return false
+      })
+    })
+  let container = []
+  assets.forEach(asset => {
+    container.push([asset, dirObj[path.join(asset, '..')]])
+  })
+  async.parallel(
+    container.map(([asset, target]) => callback => {
+      fs.copy(
+        asset,
+        path.join(root, dest, target, path.basename(asset)),
+        callback
+      )
+    }),
+    (err, _results) => {
       if (err) return callback(err)
+      logger.verbose(
+        `File assets copied in ${prettyTime(performance.now() - begin)}`
+      )
       return callback(null, conn)
     }
   )
