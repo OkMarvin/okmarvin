6e6ef7fd2f403d2bfaecddd538bf01dc2c154cf5
chenxsan <chenxsan@gmail.com>
2019-01-15 22:41:04 +0800

    Optimize

--- packages/okmarvin/lib/output/renderMarkdown/index.js
+++ packages/okmarvin/lib/output/renderMarkdown/index.js
@@ -16,14 +16,19 @@ module.exports = function (conn, callback) {
     function (file, callback) {
       // composed page do not have `content`
       // we need to prerender `content` as feed/sitemap need
-      const content = file.content ? MD.render(file.content) : ''
+      const { content = '', ...others } = file
 
-      callback(null, { ...file, content }) // we should keep rendered content or feed will have raw markdown
+      callback(null, {
+        ...others,
+        content: content ? MD.render(content) : ''
+      }) // we should keep rendered content or feed will have raw markdown
     },
     function (err, files) {
       if (err) return callback(err)
       logger.success(
-        `Rendered ${files.length} markdown files in ${prettyTime(Date.now() - begin)}`
+        `Rendered ${files.length} markdown files in ${prettyTime(
+          Date.now() - begin
+        )}`
       )
       callback(null, {
         ...others,
