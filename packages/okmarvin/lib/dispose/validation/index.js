f52c75fb198bf6c9b6388841ab0bee8cdcd7314d
chenxsan <chenxsan@gmail.com>
2019-01-15 22:11:24 +0800

    Optimize

--- packages/okmarvin/lib/dispose/validation/index.js
+++ packages/okmarvin/lib/dispose/validation/index.js
@@ -1,33 +1,21 @@
 const groupBy = require('lodash/fp/groupby')
-const async = require('async')
-function print (arr) {
-  return arr.map(i => `\n` + i.filePath || i.title)
-}
+const logger = require('@parcel/logger')
 module.exports = function (conn, callback) {
   const { files } = conn
   const groupsByPermalink = groupBy(file => file.permalink, files)
-  async.parallel(
-    [
-      callback => {
-        async.each(
-          Object.keys(groupsByPermalink),
-          (key, callback) => {
-            if (groupsByPermalink[key].length > 1) {
-              return callback(
-                new Error(
-                  `Please fix the duplicated permalinks: ${print(groupsByPermalink[key])}`
-                )
-              )
-            }
-            return callback()
-          },
-          callback
-        )
-      }
-    ],
-    err => {
-      if (err) return callback(err)
-      callback(null, conn)
-    }
-  )
+  const errors = Object.entries(groupsByPermalink)
+    .filter(([_permalink, items]) => items.length > 1)
+    .reduce((acc, [permalink, items]) => {
+      acc.set(permalink, items.map(item => item.filePath || item.title))
+      return acc
+    }, new Map())
+
+  if (errors.size) {
+    logger.error(`Duplicate permalinks detected, please fix:`)
+    errors.forEach((value, key) => {
+      console.log(key, value)
+    })
+    process.exit(1)
+  }
+  callback(null, conn)
 }
