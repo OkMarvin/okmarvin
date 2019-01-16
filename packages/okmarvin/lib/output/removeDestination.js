53b55d2dba768cebb54675911a45d5339a4aadc5
chenxsan <chenxsan@gmail.com>
2019-01-16 10:56:01 +0800

    Refactor

--- packages/okmarvin/lib/output/removeDestination.js
+++ packages/okmarvin/lib/output/removeDestination.js
@@ -1,12 +1,10 @@
 const fs = require('fs-extra')
 const path = require('path')
 const logger = require('@parcel/logger')
-module.exports = async function (conn, callback) {
-  try {
-    await fs.emptyDir(path.join(conn.root, conn.dest))
+module.exports = function (conn, callback) {
+  fs.emptyDir(path.join(conn.root, conn.dest), err => {
+    if (err) return callback(err)
     logger.verbose(`Cleaned '${conn.dest}' directory`)
-    callback(null, conn)
-  } catch (err) {
-    callback(null, conn)
-  }
+    callback(null)
+  })
 }
