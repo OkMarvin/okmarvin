a1e8c1adc5aa960f45c23552fa085d3f12ffc876
chenxsan <chenxsan@gmail.com>
2019-01-21 15:16:21 +0800

    Optimize

--- packages/okmarvin/lib/index.js
+++ packages/okmarvin/lib/index.js
@@ -3,15 +3,14 @@
 const async = require('neo-async')
 const logger = require('@parcel/logger')
 const { resolve } = require('path')
+const { prettyTime } = require('@okmarvin/helpers')
 
 const read = require('./read')
+const parseFiles = require('./parseFiles')
 const dispose = require('./dispose')
 const output = require('./output')
 
-const { prettyTime } = require('@okmarvin/helpers')
 const logMemoryUsage = require('./helpers/logMemoryUsage')
-
-const parseFiles = require('./parseFiles')
 
 /**
  * An opinionated static site generator with Component as template.
@@ -22,6 +21,8 @@ const parseFiles = require('./parseFiles')
  * @property {function|boolean} opts.devHook - Hook for dev env
  * @property {number} opts.logLevel  - Log level
  * @property {boolean} opts.clean  - Enable incremental rebuild
+ * @property {boolean} opts.builtAt
+ *
  * @property {function} callbackFn - callback function
  */
 module.exports = function okmarvin (
@@ -41,12 +42,12 @@ module.exports = function okmarvin (
   logger.setOptions({ logLevel })
 
   if (clean === false) {
-    // warn user the possible bugs `false` incremental rebuild might bring
+    // warn user the possible bugs incremental rebuild might bring
     logger.warn('Incremental rebuild is not stable yet!!')
   }
 
   if (resolve(root, dest) === root) {
-    return logger.error(`'dest' cannot be set to current working directory`)
+    return logger.error(`'dest' option cannot be set to current working directory`)
   }
 
   // connection
@@ -61,11 +62,11 @@ module.exports = function okmarvin (
   let tasks = [async.constant(conn), read, parseFiles]
 
   devHook
-    ? (tasks = [...tasks, devHook])
+    ? (tasks = [...tasks, devHook]) // we'll let browser handle `dispose` in dev env
     : (tasks = [...tasks, dispose, output])
 
   // default callback for async waterfall
-  function callback (err, _conn) {
+  function callback (err, _conn) { // we prefix conn with _ since we won't use it
     if (err) return logger.error(err)
 
     logMemoryUsage()
