47a534befa9cab528bd9e79b48183276f637470f
chenxsan <chenxsan@gmail.com>
2019-01-23 15:29:16 +0800

    Use Object instead of Map

--- packages/okmarvin/lib/dispose/validation/index.js
+++ packages/okmarvin/lib/dispose/validation/index.js
@@ -1,19 +1,19 @@
 'use strict'
 
 const groupBy = require('lodash/fp/groupby')
-module.exports = (conn) => {
+module.exports = conn => {
   const { files } = conn
   const groupsByPermalink = groupBy(file => file.permalink, files)
   const errors = Object.entries(groupsByPermalink)
     .filter(([_permalink, items]) => items.length > 1)
     .reduce((acc, [permalink, items]) => {
-      acc.set(permalink, items.map(item => item.filePath || item.title))
+      acc[permalink] = items.map(item => item.filePath || item.title)
       return acc
-    }, new Map())
+    }, {})
 
-  if (errors.size) {
+  if (Object.keys(errors).length) {
     console.error(`Duplicate permalinks detected, please fix:`)
-    return errors.forEach((value, key) => {
+    return Object.entries(errors).forEach(([key, value]) => {
       console.log(key, value)
     })
   }
