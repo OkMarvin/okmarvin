8de8769281a549a9b8beee43a73862a52f5ec541
chenxsan <chenxsan@gmail.com>
2019-01-23 21:16:27 +0800

    Remove neo-async

--- packages/okmarvin/lib/output/index.js
+++ packages/okmarvin/lib/output/index.js
@@ -11,7 +11,7 @@ module.exports = (conn, callback) => {
     async.constant(conn),
     prepare,
     compute,
-    renderMarkdown,
+    (conn, callback) => callback(null, renderMarkdown(conn)),
     write,
     copy,
     cleanup
--- packages/okmarvin/lib/output/renderMarkdown/index.js
+++ packages/okmarvin/lib/output/renderMarkdown/index.js
@@ -1,40 +1,27 @@
 'use strict'
-const async = require('neo-async')
+
 const logger = require('@parcel/logger')
 
 const { prettyTime } = require('@okmarvin/helpers')
 
 const md = require('@okmarvin/markdown')
 
-module.exports = function (conn, callback) {
+module.exports = function(conn) {
   const begin = Date.now()
   const { files, okmarvinConfig, ...others } = conn
   const MD = md(okmarvinConfig)
-
-  async.map(
-    files,
-    function (file, callback) {
-      // composed page do not have `content`
-      // we need to prerender `content` as feed/sitemap need
-      const { content = '', ...others } = file
-
-      callback(null, {
-        ...others,
-        content: content ? MD.render(content) : ''
-      }) // we should keep rendered content or feed will have raw markdown
-    },
-    function (err, files) {
-      if (err) return callback(err)
-      logger.success(
-        `Rendered ${files.length} markdown files in ${prettyTime(
-          Date.now() - begin
-        )}`
-      )
-      callback(null, {
-        ...others,
-        okmarvinConfig,
-        files
-      })
-    }
+  const newFiles = files.reduce((acc, file) => {
+    const { content = '', ...others } = file
+    return [...acc, { ...others, content: content ? MD.render(content) : '' }]
+  }, [])
+  logger.success(
+    `Rendered ${files.length} markdown files in ${prettyTime(
+      Date.now() - begin
+    )}`
   )
+  return {
+    ...others,
+    okmarvinConfig,
+    files: newFiles
+  }
 }
