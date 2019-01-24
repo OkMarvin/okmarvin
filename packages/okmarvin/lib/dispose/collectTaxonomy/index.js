12857665265648cf21fef868f9ac4998f79e6a1e
chenxsan <chenxsan@gmail.com>
2019-01-24 10:06:15 +0800

    Optimize performance

--- packages/okmarvin/lib/dispose/collectTaxonomy/index.js
+++ packages/okmarvin/lib/dispose/collectTaxonomy/index.js
@@ -1,9 +1,14 @@
 'use strict'
 const collector = require('./collector')
-module.exports = (conn) => {
-  const { files } = conn
+module.exports = conn => {
+  const { files, themeManifest } = conn
   const tags = collector(files, 'tags')
-  const categories = collector(files, 'categories')
-  const authors = collector(files, 'author')
-  return { ...conn, tags, categories, authors }
+  let results = { ...conn, tags }
+  if (themeManifest['category.js']) {
+    results = { ...results, categories: collector(files, 'categories') }
+  }
+  if (themeManifest['author.js']) {
+    results = { ...results, authors: collector(files, 'author') }
+  }
+  return results
 }
