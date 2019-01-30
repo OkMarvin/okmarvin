1fc0d60316cd3e05cab28a205ca35cae0f43fd28
chenxsan <chenxsan@gmail.com>
2019-01-30 21:15:50 +0800

    Fix for windows os

--- packages/okmarvin/lib/parseFiles/parseFile/getInferredTemplate.js
+++ packages/okmarvin/lib/parseFiles/parseFile/getInferredTemplate.js
@@ -2,7 +2,6 @@
  * Say file exists under post/hello.md
  * then we should set template to `post.js`
  */
-const path = require('path')
 module.exports = function (filePath) {
-  return filePath.split(path.sep)[0].slice(1, -1) + '.js'
+  return filePath.split('/')[0].slice(1, -1) + '.js'
 }
