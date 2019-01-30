7683076886f2b397310b5bed461302fe1ba1f82d
Sam Chen <chenxsan@gmail.com>
2019-01-30 22:10:22 +0800

    Fix test for windows os

--- packages/okmarvin/lib/output/copy/copyFileAssets/findMyRoot.test.js
+++ packages/okmarvin/lib/output/copy/copyFileAssets/findMyRoot.test.js
@@ -1,29 +1,30 @@
+const path = require('path')
 const findMyRoot = require('./findMyRoot')
 const fileAssetRootPermalinkMap = {
-  '_pages/about': '/about/',
-  '_posts/hello-okmarvin': '/hello-okmarvin/'
+  [path.join('_pages/about')]: '/about/',
+  [path.join('_posts/hello-okmarvin')]: '/hello-okmarvin/'
 }
 test('', () => {
   expect(
     findMyRoot(
-      '_pages/about/a.txt',
+      path.join('_pages/about/a.txt'),
       Object.keys(fileAssetRootPermalinkMap)
     )
-  ).toEqual(['_pages/about', 'a.txt'])
+  ).toEqual([path.join('_pages/about'), 'a.txt'])
 })
 test('', () => {
   expect(
     findMyRoot(
-      '_pages/about/demo/a.txt',
+      path.join('_pages/about/demo/a.txt'),
       Object.keys(fileAssetRootPermalinkMap)
     )
-  ).toEqual(['_pages/about', 'demo/a.txt'])
+  ).toEqual([path.join('_pages/about'), path.join('demo/a.txt')])
 })
 test('', () => {
   expect(
     findMyRoot(
-      '_pages/about/demo/demo/a.txt',
+      path.join('_pages/about/demo/demo/a.txt'),
       Object.keys(fileAssetRootPermalinkMap)
     )
-  ).toEqual(['_pages/about', 'demo/demo/a.txt'])
+  ).toEqual([path.join('_pages/about'), path.join('demo/demo/a.txt')])
 })
