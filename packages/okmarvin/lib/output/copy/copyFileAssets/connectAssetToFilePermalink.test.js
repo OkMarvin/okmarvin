c08a56b1f97971843fc85c225f05a91333a6864e
Sam Chen <chenxsan@gmail.com>
2019-01-30 22:20:31 +0800

    Fix tests for windows os

--- packages/okmarvin/lib/output/copy/copyFileAssets/connectAssetToFilePermalink.test.js
+++ packages/okmarvin/lib/output/copy/copyFileAssets/connectAssetToFilePermalink.test.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const connectAssetToFile = require('./connectAssetToFilePermalink')
 test('', () => {
   expect(
-    connectAssetToFile(['_posts/why/demo/index.html'], {
-      '_posts/why': '/why-okmarvin'
+    connectAssetToFile([path.join('_posts/why/demo/index.html')], {
+      [path.join('_posts/why')]: '/why-okmarvin'
     }).sort()
   ).toEqual(
-    [['_posts/why/demo/index.html', '/why-okmarvin/demo/index.html']].sort()
+    [[path.join('_posts/why/demo/index.html'), path.join('/why-okmarvin/demo/index.html')]].sort()
   )
 })
--- packages/okmarvin/lib/output/copy/copyFileAssets/findPotentialAssetRootPermalinkMap.test.js
+++ packages/okmarvin/lib/output/copy/copyFileAssets/findPotentialAssetRootPermalinkMap.test.js
@@ -1,22 +1,23 @@
+const path = require('path')
 const findPotentialAssetRootPermalinkMap = require('./findPotentialAssetRootPermalinkMap')
 test('', () => {
   expect(
     findPotentialAssetRootPermalinkMap([
       {
-        filePath: '_posts/hello/index.md',
+        filePath: path.join('_posts/hello/index.md'),
         permalink: '/'
       },
       {
-        filePath: '_pages/test.md',
+        filePath: path.join('_pages/test.md'),
         permalink: '/test'
       }
     ])
-  ).toEqual({ '_posts/hello': '/' })
+  ).toEqual({ [path.join('_posts/hello')]: '/' })
 })
 test('we wont allow file assets right under _posts', () => {
   expect(
     findPotentialAssetRootPermalinkMap([{
-      filePath: '_posts/om.md',
+      filePath: path.join('_posts/om.md'),
       permalink: '/om'
     }])
   ).toEqual({})
