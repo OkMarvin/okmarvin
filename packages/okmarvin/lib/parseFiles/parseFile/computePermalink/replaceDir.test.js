572f968c69448ec0c75ca25f1da7220bbf129768
chenxsan <chenxsan@gmail.com>
2019-02-01 23:21:37 +0800

    Fix test

--- packages/okmarvin/lib/parseFiles/parseFile/computePermalink/replaceDir.test.js
+++ packages/okmarvin/lib/parseFiles/parseFile/computePermalink/replaceDir.test.js
@@ -1,15 +1,15 @@
 const fn = require('./replaceDir')
 const path = require('path')
 test(':dir replaced', () => {
-  expect(fn('/:dir', path.join('page/blog/hello'))).toBe('/blog/hello')
+  expect(fn('/:dir', path.join('_pages/blog/hello'))).toBe('/blog/hello')
 })
 test(':dir with chinese', () => {
   expect(
-    fn('/:dir', path.join('page/博客/测试'))
+    fn('/:dir', path.join('_pages/博客/测试'))
   ).toBe(`/${encodeURIComponent('博客')}/${encodeURIComponent('测试')}`)
 })
 test(':dir with chinese and whitespace', () => {
   expect(
-    fn('/:dir', path.join('page/博 客/测试'))
+    fn('/:dir', path.join('_pages/博 客/测试'))
   ).toBe(`/${encodeURIComponent('博-客')}/${encodeURIComponent('测试')}`)
 })
