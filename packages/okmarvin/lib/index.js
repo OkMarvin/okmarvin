fa474526052e483a217604a13bc1cf318e52dcb6
chenxsan <chenxsan@gmail.com>
2019-06-09 19:57:47 +0800

    Clean devHook

--- packages/okmarvin/lib/index.js
+++ packages/okmarvin/lib/index.js
@@ -16,22 +16,19 @@ const logMemoryUsage = require('./helpers/logMemoryUsage')
  * An opinionated static site generator with Component as template.
  * @function okmarvin
  * @property {object} opts  - Options for okmarvin
- * @property {string} opts.root  - Root path
- * @property {string} opts.dest  - Destination directory
- * @property {function|boolean} opts.devHook - Hook for dev env
- * @property {number} opts.logLevel  - Log level
- * @property {boolean} opts.builtAt
+ * @property {string} opts.root  - Root path, defaults to current working directory
+ * @property {string} opts.dest  - Where to output site
+ * @property {number} opts.logLevel  - Log level, check https://parceljs.org/cli.html#change-log-level for detail
+ * @property {boolean} opts.builtAt - When the build starts
  *
- * @property {function} callbackFn - callback function
+ * @property {function} callbackFn - Function to run at the end
  */
 const okmarvin = (
   {
     root = process.cwd(),
     dest = '_site',
-    devHook = false,
     logLevel = 3,
-    builtAt = Date.now(),
-    benchmark = false
+    builtAt = Date.now()
   } = {},
   callbackFn
 ) => {
@@ -40,28 +37,27 @@ const okmarvin = (
   logger.setOptions({ logLevel })
 
   if (resolve(root, dest) === root) {
-    return logger.error(
-      `'dest' option cannot be set to current working directory`
-    )
+    return logger.error(`'dest' cannot be set to current working directory`)
   }
 
   // connection (get this name from PhoenixFramework)
   const conn = {
     root,
     dest,
-    devHook,
     builtAt
   }
 
-  let tasks = [async.constant(conn), read, parseFiles]
-
-  devHook
-    ? (tasks = [...tasks, devHook])
-    : (tasks = [...tasks, (conn, callback) => callback(null, dispose(conn)), output])
+  const tasks = [
+    async.constant(conn),
+    read,
+    parseFiles,
+    (conn, callback) => callback(null, dispose(conn)),
+    output
+  ]
 
   // default callback for async waterfall
   const callback = (err, _conn) => {
-    // we prefix conn with _ since we won't use it, idea from elixir
+    // we prefix conn with _ since we won't use it
     if (err) return logger.error(err)
 
     logMemoryUsage()
