b0181dba7d0068d3873964045cf64147eb1ada7e
chenxsan <chenxsan@gmail.com>
2019-07-02 21:35:44 +0800

    Use path.sep

--- packages/okmarvin/lib/parseSourceFiles/parseSourceFile/getInferredTemplate.js
+++ packages/okmarvin/lib/parseSourceFiles/parseSourceFile/getInferredTemplate.js
@@ -1,7 +1,9 @@
+'use strict'
 /**
  * Say file exists under post/hello.md
  * then we should set template to `post.js`
  */
-module.exports = function (filePath) {
-  return filePath.split('/')[0].slice(1, -1) + '.js'
+const { sep } = require('path')
+module.exports = function(filePath) {
+  return filePath.split(sep)[0].slice(1, -1) + '.js'
 }
